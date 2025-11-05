100845dd00aac67eed91aac80c342b845454687d
lohitkk <167980105+lohitkk@users.noreply.github.com>
2025-11-05 14:35:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"   # You can change this if you’re using another region
+  region     = var.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "resume_matcher" {
   ami           = "ami-0c2b8ca1dad447f8a"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "ResumeMatcher"
@@ -16,11 +16,12 @@ resource "aws_instance" "resume_matcher" {
               #!/bin/bash
               yum update -y
               yum install -y docker
-              service docker start
+              systemctl start docker
+              systemctl enable docker
               docker run -d -p 3000:3000 lohitkk/resume-matcher:latest
               EOF
 }
 
 output "instance_public_ip" {
-  value = aws_instance.resume_matcher.public_ip
-}
+  value       = aws_instance.resume_matcher.public_ip
+  description = "Public IP of Resume Matcher EC2 instance
