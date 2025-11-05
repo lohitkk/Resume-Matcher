0a47c39480d00fb36a74ee3551874382f2eddf27
lohitkk <167980105+lohitkk@users.noreply.github.com>
2025-11-05 14:35:53 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.micro"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
